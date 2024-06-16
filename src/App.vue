b8d80442cf2030100bcb72c0813b13a13a420ca8
1064 <1064@student.itgymnazium.cz>
2024-06-16 21:52:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Seznam úkolů</h1>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+}
+</style>
